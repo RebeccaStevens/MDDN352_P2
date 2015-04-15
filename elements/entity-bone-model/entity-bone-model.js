a454ce9a77dfebd72e104f792053b9bc9e04496e
slaymaster3000 <slaymaster3000@gmail.com>
2015-04-15 14:14:36 +1200

    refactored the friction variable

--- elements/entity-bone-model/entity-bone-model.js
+++ elements/entity-bone-model/entity-bone-model.js
@@ -11,23 +11,27 @@ Polymer({
 	    vel_x: 0,
 	    vel_y: 0,
 	    accel_x: 0,
-	    accel_y: 0
+	    accel_y: 0,
+	    friction: 0
 	},
 	ready: function(){
 		
 	},
-	animate: function(time_step, friction){
-		if(friction == 0) {
+	setFriction: function(val){
+		this.friction = val;
+	},
+	animate: function(time_step){
+		if(this.friction == 0) {
 			this.x += this.vel_x * time_step + 0.5 * this.accel_x * time_step * time_step;
 			this.y += this.vel_y * time_step + 0.5 * this.accel_y * time_step * time_step;
 			this.vel_x = this.vel_x + this.accel_x * time_step;
 			this.vel_y = this.vel_y + this.accel_y * time_step;
 		}
 		else {
-			this.x += this.accel_x * time_step / friction + (this.vel_x - this.accel_x / friction) * (1 - Math.exp(-friction * time_step)) / friction
-			this.y += this.accel_y * time_step / friction + (this.vel_y - this.accel_y / friction) * (1 - Math.exp(-friction * time_step)) / friction
-			this.vel_x = this.accel_x / friction + (this.vel_x - this.accel_x / friction) * Math.exp(-friction * time_step);
-			this.vel_y = this.accel_y / friction + (this.vel_y - this.accel_y / friction) * Math.exp(-friction * time_step);
+			this.x += this.accel_x * time_step / this.friction + (this.vel_x - this.accel_x / this.friction) * (1 - Math.exp(-this.friction * time_step)) / this.friction
+			this.y += this.accel_y * time_step / this.friction + (this.vel_y - this.accel_y / this.friction) * (1 - Math.exp(-this.friction * time_step)) / this.friction
+			this.vel_x = this.accel_x / this.friction + (this.vel_x - this.accel_x / this.friction) * Math.exp(-this.friction * time_step);
+			this.vel_y = this.accel_y / this.friction + (this.vel_y - this.accel_y / this.friction) * Math.exp(-this.friction * time_step);
 			if(Math.abs(this.vel_x) < 0.0000000000001){
 				this.vel_x = 0;
 			}
